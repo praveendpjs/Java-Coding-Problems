0968a42355302df32c9f8c757894af1ce5dd2bcd
praveendpjs <praveenaishu2012@gmail.com>
2026-01-01 12:00:46 +0530

    commit

---
+++ Zohoquestions/RotationString.java
@@ -0,0 +1,14 @@
+// abcd rotational string would be bcda, cdab, dabc
+// https://www.youtube.com/watch?v=wFxtrAnpxxg
+public class RotationString {
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "bcda";
+        System.out.println(isRotation(s1,s2));
+        
+
+    }
+    public static boolean isRotation(String s1, String s2){
+        return s1.length() == s2.length() && (s1+s1).contains(s2);
+    }
+}
