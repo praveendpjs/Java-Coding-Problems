693c5d455e084562102cade6e55b7d332d49386c
praveendpjs <praveenaishu2012@gmail.com>
2026-02-20 08:55:32 +0530

    Create SummaryRanges.java

---
+++ Leetcode/EasyArray/SummaryRanges.java
@@ -0,0 +1,69 @@
+// https://leetcode.com/problems/summary-ranges/description/?envType=problem-list-v2&envId=wafs0t6v
+// 228. Summary Ranges
+
+// You are given a sorted unique integer array nums.
+
+// A range [a,b] is the set of all integers from a to b (inclusive).
+
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+// Each range [a,b] in the list should be output as:
+
+// "a->b" if a != b
+// "a" if a == b
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,2,4,5,7]
+// Output: ["0->2","4->5","7"]
+// Explanation: The ranges are:
+// [0,2] --> "0->2"
+// [4,5] --> "4->5"
+// [7,7] --> "7"
+// Example 2:
+
+// Input: nums = [0,2,3,4,6,8,9]
+// Output: ["0","2->4","6","8->9"]
+// Explanation: The ranges are:
+// [0,0] --> "0"
+// [2,4] --> "2->4"
+// [6,6] --> "6"
+// [8,9] --> "8->9"
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        int start = nums[0];  // beginning of current range
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // check if end of range
+            if (i == nums.length - 1 || nums[i] + 1 != nums[i + 1]) {
+
+                int end = nums[i];
+
+                if (start == end) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + end);
+                }
+
+                // move start to next element (if exists)
+                if (i != nums.length - 1) {
+                    start = nums[i + 1];
+                }
+            }
+        }
+
+        return result;
+    }
+}
