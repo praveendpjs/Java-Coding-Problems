a04085db8aa9ebc2722ed86b2eb1d51903a925ea
praveendpjs <praveenaishu2012@gmail.com>
2026-02-22 07:50:54 +0530

    Create CanPlaceFlower.java

---
+++ Leetcode/EasyArray/CanPlaceFlower.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/can-place-flowers/description/?envType=problem-list-v2&envId=wafs0t6v
+// 605. Can Place Flowers
+
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+public class CanPlaceFlower {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = n;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if (flowerbed[i] == 0) {
+
+                int left = (i == 0) ? 0 : flowerbed[i - 1];
+                int right = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+
+                if (left == 0 && right == 0) {
+                    flowerbed[i] = 1;
+                    count--;
+                }
+            }
+        }
+
+        return count <= 0;
+    }
+}
