88713ea54ffb4215f850e72b36e15a4a6bba295a
praveendpjs <praveenaishu2012@gmail.com>
2026-03-15 14:53:55 +0530

    push

--- Leetcode/EasyStrings/RansomNote.java
+++ Leetcode/EasyStrings/RansomNote.java
@@ -49,3 +49,23 @@ public class RansomNote {
         return true;
     }
 }
+
+// public boolean canConstruct(String ransomNote, String magazine) {
+//         Map<Character, Integer> map = new HashMap<>();
+
+//         // Count characters in magazine
+//         for (char c : magazine.toCharArray()) {
+//             map.put(c, map.getOrDefault(c, 0) + 1);
+//         }
+
+//         // Try to build ransomNote
+//         for (char c : ransomNote.toCharArray()) {
+//             if (!map.containsKey(c) || map.get(c) == 0) {
+//                 return false;
+//             }
+
+//             map.put(c, map.get(c) - 1);
+//         }
+
+//         return true;
+//     }
